1efb6eddbf4ea2acd97bbee0809a11ddfc5031e2
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-08-15 11:57:00 +0200

    working at moving all linked dependencies to shared

---
+++ linker/src/main.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+
+use tokio::{
+    fs::{read_dir, try_exists, write, File},
+    io::AsyncReadExt,
+};
+
+#[tokio::main]
+async fn main() {
+    let mut file = File::open("main.Cargo.toml")
+        .await
+        .expect("Did not find preset cargo file");
+    let mut str = String::new();
+    file.read_to_string(&mut str)
+        .await
+        .expect("Unable to read preset cargo file to string");
+    let mut timeline_location = String::new();
+    let mut timeline_location_file = File::open("../timeline_location.txt")
+        .await
+        .expect("Did not find timeline location file!");
+    timeline_location_file
+        .read_to_string(&mut timeline_location)
+        .await
+        .expect("Unable to read timeline location file!");
+
+    let timeline_directory = PathBuf::from("../").join(PathBuf::from(timeline_location));
+
+    str += &format!(
+        "\ntypes = {{path = \"{}\"}}\n",
+        timeline_directory.join("types").display()
+    );
+
+    write("../shared/Cargo.toml", str)
+        .await
+        .expect("Unable to write new Cargo.toml file");
+}
