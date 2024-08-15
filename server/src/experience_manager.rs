03dfabb9e0f8d1ce7fe61f2d4bdec7af92978899
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-08-15 15:46:21 +0200

    experience loading should be working

--- server/src/experience_manager.rs
+++ server/src/experience_manager.rs
@@ -1,16 +1,17 @@
 use std::{
     collections::{HashMap, HashSet},
     path::{Path, PathBuf},
+    sync::Arc,
 };
 
 use shared::types::{Experience, ExperienceError, ExperienceResult};
-use tokio::sync::RwLock;
+use tokio::{fs::File, io::AsyncReadExt, sync::RwLock};
 
 use crate::config::Config;
 
 pub struct ExperienceManager {
     folder: PathBuf,
-    cache: RwLock<HashMap<String, RwLock<Experience>>>,
+    cache: RwLock<HashMap<String, Arc<RwLock<Experience>>>>,
 }
 
 impl ExperienceManager {
@@ -22,13 +23,36 @@ impl ExperienceManager {
     }
 
     pub async fn get_experience(&self, id: &str) -> ExperienceResult<Experience> {
+        let mut found_experience;
         {
             let cache = self.cache.read().await;
-            if let Some(v) = cache.get(id) {
-                return Ok(v.read().await.clone());
-            }
+            found_experience = cache.get(id).cloned();
         }
 
-        unimplemented!()
+        match found_experience {
+            Some(v) => Ok(v.read().await.clone()),
+            None => {
+                let path = self.folder.join(format!("{}.experience.json", id));
+                let mut file = match File::open(path).await {
+                    Ok(v) => v,
+                    Err(e) => return Err(ExperienceError::NotFound(e.to_string())),
+                };
+                let mut experience_file_content = String::new();
+                if let Err(e) = file.read_to_string(&mut experience_file_content).await {
+                    return Err(ExperienceError::FileError(e.to_string()));
+                }
+
+                let experience: Experience = serde_json::from_str(&experience_file_content)?;
+
+                {
+                    self.cache
+                        .write()
+                        .await
+                        .insert(id.to_string(), Arc::new(RwLock::new(experience.clone())));
+                }
+
+                Ok(experience)
+            }
+        }
     }
 }
--- shared/src/types.rs
+++ shared/src/types.rs
@@ -1,22 +1,30 @@
 use std::{collections::HashMap, error, fmt};
 
+use serde::{Deserialize, Serialize};
+
 use crate::timeline::types::api::{AvailablePlugins, CompressedEvent};
 
-#[derive(Debug, Clone)]
+#[cfg_attr(feature = "server", derive(Serialize))]
+#[derive(Debug, Clone, Deserialize)]
 pub struct ExperienceEvent {
     pub favorite: bool,
     pub event: CompressedEvent,
 }
 
-#[derive(Debug, Clone)]
+#[cfg_attr(feature = "server", derive(Serialize))]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Experience {
     pub events: HashMap<AvailablePlugins, Vec<ExperienceEvent>>,
     pub public: bool,
 }
 
+#[cfg_attr(feature = "server", derive(Serialize))]
+#[cfg_attr(feature = "client", derive(Deserialize))]
 #[derive(Debug)]
 pub enum ExperienceError {
     NotFound(String),
+    FileError(String),
+    ParsingError(String),
     UnableToWrite(String),
 }
 
@@ -27,6 +35,8 @@ impl fmt::Display for ExperienceError {
             ExperienceError::UnableToWrite(v) => {
                 write!(f, "Unable to write experience to file: {}", v)
             }
+            ExperienceError::ParsingError(v) => write!(f, "Unable to parse data: {}", v),
+            ExperienceError::FileError(v) => write!(f, "Unable to read the file correctly: {}", v),
         }
     }
 }
@@ -34,3 +44,10 @@ impl fmt::Display for ExperienceError {
 pub type ExperienceResult<T> = Result<T, ExperienceError>;
 
 impl error::Error for ExperienceError {}
+
+#[cfg(feature = "server")]
+impl From<serde_json::Error> for ExperienceError {
+    fn from(value: serde_json::Error) -> Self {
+        ExperienceError::ParsingError(value.to_string())
+    }
+}
