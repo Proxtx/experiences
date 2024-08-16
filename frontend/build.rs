2c6879dbba1b282c7d08a4b1fca740788f9dfedf
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-08-16 20:54:42 +0200

    setup more fronted stuff

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,5 +1,5 @@
 use stylers::build;
 
 fn main() {
-    build(Some(String::from("./target/main.css")));
+    build(Some(String::from("./target/generated.css")));
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|| {
+        view! {
+            hi
+        }
+    })
+}
+
+#[component]
+fn TitleBar(#[prop(into)] title: MaybeSignal<String>) -> impl IntoView {
+    view! {}
 }
