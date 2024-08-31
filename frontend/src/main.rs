ee7b541a2f0d175dfae1a08b9bd6e353aeaa46ab
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-08-31 23:14:33 +0200

    fix scroll overflow hopefully

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -150,7 +150,7 @@ fn ExperienceView() -> impl IntoView {
                                             Some(v) => {
                                                 provide_context(v);
                                                 view! {
-                                                    <div style="overflow: auto;display: flex;flex-direction: column;" on:click=move |_| {write_navigator_expanded(false)}>
+                                                    <div style="height: 100%; overflow: auto;display: flex;flex-direction: column;" on:click=move |_| {write_navigator_expanded(false)}>
                                                         <experience::Experience id=experience_id></experience::Experience>
                                                     </div>
                                                 }.into_view()
